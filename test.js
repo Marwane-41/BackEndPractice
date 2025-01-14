487aa8ca1ffb1b578cc6d9d05bed5f2e1589f406
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2025-01-14 10:52:07 -0700

    Learning about dependenecies

---
+++ test.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+
+var spiralOrder = function(matrix) {
+        
+    // create length for the variables : 
+   
+    let x = matrix.length;
+    let y = matrix[0].length;
+    
+    console.log(x);
+    console.log(y);
+
+    console.log(matrix[1]);
+};
+
+
+spiralOrder([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
